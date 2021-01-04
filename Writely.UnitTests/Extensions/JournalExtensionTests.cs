2d32b072945318d3917ff2bebf9abc2e5674fc79
Kianek <kallridge92@gmail.com>
2021-01-04 12:28:06 -0800

    Passing test

--- Writely.UnitTests/Extensions/JournalExtensionTests.cs
+++ Writely.UnitTests/Extensions/JournalExtensionTests.cs
@@ -1,9 +1,45 @@
+using System.Collections.Generic;
+using System.Linq;
 using FluentAssertions;
+using Writely.Extensions;
+using Writely.Models;
+using Writely.Models.Dto;
 using Xunit;
 
 namespace Writely.UnitTests.Extensions
 {
     public class JournalExtensionTests
     {
+        [Fact]
+        public void MapToDto_ReturnsListOfLength3()
+        {
+            // Arrange
+            var userId = "UserId";
+            var journals = new List<Journal>
+            {
+                new Journal
+                {
+                    Title = "Journal 1",
+                    UserId = userId
+                },
+                new Journal
+                {
+                    Title = "Journal 2",
+                    UserId = userId
+                },
+                new Journal
+                {
+                    Title = "Journal 3",
+                    UserId = userId
+                },
+            };
+            
+            // Act
+            var result = journals.MapToDto();
+            
+            // Assert
+            result.Should().BeOfType<List<JournalDto>>();
+            result.Count().Should().Be(3);
+        }
     }
 }
