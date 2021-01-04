745230448d29c16d9cf90d2b52f857aca4ed570b
Kianek <kallridge92@gmail.com>
2021-01-04 11:04:25 -0800

    Add UserId property and constructors

--- Writely/Data/Entity.cs
+++ Writely/Data/Entity.cs
@@ -5,7 +5,17 @@ namespace Writely.Data
     public class Entity
     {
         public long Id { get; set; }
+        public string UserId { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime LastModified { get; set; }
+
+        public Entity(string userId)
+        {
+            UserId = userId;
+        }
+
+        public Entity()
+        {
+        }
     }
 }
