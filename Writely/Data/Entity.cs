d41e77175f5a8978e3fa759de22b3fe8d0fb7792
Kianek <kallridge92@gmail.com>
2021-01-04 11:54:23 -0800

    Remove extraneous empty constructor

--- Writely/Data/Entity.cs
+++ Writely/Data/Entity.cs
@@ -5,17 +5,13 @@ namespace Writely.Data
     public class Entity
     {
         public long Id { get; set; }
-        public string UserId { get; set; }
+        public string? UserId { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime LastModified { get; set; }
 
-        public Entity(string userId)
+        public Entity(string? userId = null)
         {
             UserId = userId;
-        }
-
-        public Entity()
-        {
         }
     }
 }
