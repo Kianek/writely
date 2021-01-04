65726dc2f62dfb5f6ef9c198584a2b886af0e072
Kianek <kallridge92@gmail.com>
2021-01-04 11:54:58 -0800

    Add CreatedAt & LastModified properties & constructor

--- Writely/Data/AppUser.cs
+++ Writely/Data/AppUser.cs
@@ -1,9 +1,16 @@
+using System;
 using Microsoft.AspNetCore.Identity;
 
 namespace Writely.Data
 {
     public class AppUser : IdentityUser
     {
-        
+        public DateTimeOffset? CreatedAt { get; set; }
+        public DateTimeOffset? LastModified { get; set; }
+
+        public AppUser()
+        {
+            CreatedAt = LastModified = DateTimeOffset.UtcNow;
+        }
     }
 }
