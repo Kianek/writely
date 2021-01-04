3a06b1835f76c338f9ed374016f70210ba13abb5
Kianek <kallridge92@gmail.com>
2021-01-04 12:17:31 -0800

    Add empty constructor; make Title nullable

--- Writely/Models/Journal.cs
+++ Writely/Models/Journal.cs
@@ -5,12 +5,14 @@ namespace Writely.Models
 {
     public class Journal : Entity
     {
-        public string Title { get; set; }
+        public string? Title { get; set; }
         public List<Entry> Entries { get; set; } = new ();
 
         public Journal(string title, string userId) : base(userId)
         {
             Title = title;
         }
+        
+        public Journal() {}
     }
 }
