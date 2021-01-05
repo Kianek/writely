d582bcd88b117cab21066f19391047a0090a7046
Kianek <kallridge92@gmail.com>
2021-01-05 07:31:15 -0800

    Add null-checking

--- Writely/Services/EntryUpdater.cs
+++ Writely/Services/EntryUpdater.cs
@@ -8,19 +8,19 @@ namespace Writely.Services
         public (Entry?, bool) Update(Entry model, EntryUpdateModel updateModel)
         {
             bool didUpdate = false;
-            if (updateModel.Title is not null && model.Title != updateModel.Title)
+            if (updateModel?.Title is not null && model.Title != updateModel.Title)
             {
                 model.Title = updateModel.Title;
                 didUpdate = true;
             }
 
-            if (updateModel.Tags is not null && model.Tags != updateModel.Tags)
+            if (updateModel?.Tags is not null && model.Tags != updateModel.Tags)
             {
                 model.Tags = updateModel.Tags;
                 didUpdate = true;
             }
 
-            if (updateModel.Body is not null && model.Body != updateModel.Body)
+            if (updateModel?.Body is not null && model.Body != updateModel.Body)
             {
                 model.Body = updateModel.Body;
                 didUpdate = true;
