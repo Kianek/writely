1555b4ffb1f728e49b094f3837c40e9af2fce2cc
Kianek <kallridge92@gmail.com>
2021-01-05 07:25:02 -0800

    Passing tests

--- Writely.UnitTests/Services/JournalUpdaterTest.cs
+++ Writely.UnitTests/Services/JournalUpdaterTest.cs
@@ -1,7 +1,61 @@
+using FluentAssertions;
+using Writely.Models;
+using Writely.Services;
+using Xunit;
+
 namespace Writely.UnitTests.Services
 {
     public class JournalUpdaterTest
     {
-        
+        [Fact]
+        public void Update_ChangeTitle_ReturnsTrue()
+        {
+            // Arrange
+            var journal = Helpers.GetJournal();
+            var updateModel = new JournalUpdateModel
+            {
+                Title = "Spiffy New Title"
+            };
+            var updater = new JournalUpdater();
+            
+            // Act
+            var (updatedJournal, didUpdate) = updater.Update(journal, updateModel);
+
+            // Assert
+            didUpdate.Should().BeTrue();
+            updatedJournal.Title.Should().Be(updateModel.Title);
+        }
+
+        [Fact]
+        public void Update_TitlesIdentical_NoUpdate_ReturnsFalse()
+        {
+            // Arrange
+            var journal = Helpers.GetJournal();
+            var updateModel = new JournalUpdateModel
+            {
+                Title = journal.Title
+            };
+            var updater = new JournalUpdater();
+
+            // Act
+            var (_, didUpdate) = updater.Update(journal, updateModel);
+
+            // Assert
+            didUpdate.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Update_UpdateModelNull_ReturnsFalse()
+        {
+            // Arrange
+            var journal = Helpers.GetJournal();
+            var updater = new JournalUpdater();
+
+            // Act
+            var (_, didUpdate) = updater.Update(journal, null);
+
+            //Assert
+            didUpdate.Should().BeFalse();
+        }
     }
 }
