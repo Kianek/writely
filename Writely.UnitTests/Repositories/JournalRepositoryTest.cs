d3d7107a74458419d4aca217264e5d1f95fdec16
Kianek <kallridge92@gmail.com>
2021-01-06 09:45:05 -0800

    Initial commit

---
+++ Writely.UnitTests/Repositories/JournalRepositoryTest.cs
@@ -0,0 +1,89 @@
+using Xunit;
+
+namespace Writely.UnitTests.Repositories
+{
+    public class JournalRepositoryTest
+    {
+        [Fact]
+        public void GetById_JournalFound_ReturnsJournal()
+        {
+
+        }
+
+        [Fact]
+        public void GetById_JournalNotFound_ReturnsNull()
+        {
+            
+        }
+
+        [Fact]
+        public void GetAll_JournalsFound_NoLimit_ReturnsAllJournals()
+        {
+            
+        }
+
+        [Fact]
+        public void GetAll_JournalsFound_Limit_ReturnsLimitedJournals()
+        {
+            
+        }
+
+        [Fact]
+        public void GetAll_JournalsFound_ReturnsSortedByTitleAscending()
+        {
+            
+        }
+        
+        [Fact]
+        public void GetAll_JournalsFound_ReturnsSortedByTitleDescending()
+        {
+            
+        }
+        [Fact]
+        public void GetAll_JournalsFound_ReturnsSortedByDateAscending()
+        {
+            
+        }
+        [Fact]
+        public void GetAll_JournalsFound_ReturnsSortedByDateDescending()
+        {
+            
+        }
+
+        [Fact]
+        public void Save_JournalSaved_ReturnsJournal()
+        {
+            
+        }
+
+        [Fact]
+        public void Save_JournalNull_ReturnsNull()
+        {
+            
+        }
+
+        [Fact]
+        public void Update_JournalUpdated_ReturnsJournal()
+        {
+            
+        }
+
+        [Fact]
+        public void Update_JournalNull_ReturnsNull()
+        {
+            
+        }
+
+        [Fact]
+        public void Delete_JournalDeleted_ReturnsTrue()
+        {
+            
+        }
+
+        [Fact]
+        public void Delete_JournalNotFound_ReturnsFalse()
+        {
+            
+        }
+    }
+}
