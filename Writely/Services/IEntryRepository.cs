c22212688c4d42d525ebc529c88640039214c5fc
Kianek <kallridge92@gmail.com>
2021-01-05 16:30:43 -0800

    Minor tweaks

--- Writely/Services/IEntryRepository.cs
+++ Writely/Services/IEntryRepository.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Writely.Models;
 
@@ -5,9 +6,10 @@ namespace Writely.Services
 {
     public interface IEntryRepository
     {
-        Task<Entry> GetById(long journalId, long entryId);
-        Task<Entry> Update(IModelUpdater<Entry, EntryUpdateModel> updater, EntryUpdateModel model);
-        Task<Entry> Save(Entry entry);
+        Task<Entry> GetById(long entryId);
+        Task<List<Entry>> GetAllByJournal(long journalId);
+        Task<Entry> Save(NewEntryModel entry);
+        Task<Entry> Update(Entry entry);
         Task Delete(long journalId, long entryId);
     }
 }
