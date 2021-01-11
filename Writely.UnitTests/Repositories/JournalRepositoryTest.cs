1e387321f91a22f1941244a6cd97f43d75686ef4
Kianek <kallridge92@gmail.com>
2021-01-11 11:44:34 -0800

    Make tests async

--- Writely.UnitTests/Repositories/JournalRepositoryTest.cs
+++ Writely.UnitTests/Repositories/JournalRepositoryTest.cs
@@ -33,77 +33,77 @@ namespace Writely.UnitTests.Repositories
         }
 
         [Fact]
-        public void GetById_JournalNotFound_ReturnsNull()
+        public async Task GetById_JournalNotFound_ReturnsNull()
         {
             
         }
 
         [Fact]
-        public void GetAll_JournalsFound_NoLimit_ReturnsAllJournals()
+        public async Task GetAll_JournalsFound_NoLimit_ReturnsAllJournals()
         {
             
         }
 
         [Fact]
-        public void GetAll_JournalsFound_Limit_ReturnsLimitedJournals()
+        public async Task GetAll_JournalsFound_Limit_ReturnsLimitedJournals()
         {
             
         }
 
         [Fact]
-        public void GetAll_JournalsFound_ReturnsSortedByTitleAscending()
+        public async Task GetAll_JournalsFound_ReturnsSortedByTitleAscending()
         {
             
         }
         
         [Fact]
-        public void GetAll_JournalsFound_ReturnsSortedByTitleDescending()
+        public async Task GetAll_JournalsFound_ReturnsSortedByTitleDescending()
         {
             
         }
         [Fact]
-        public void GetAll_JournalsFound_ReturnsSortedByDateAscending()
+        public async Task GetAll_JournalsFound_ReturnsSortedByDateAscending()
         {
             
         }
         [Fact]
-        public void GetAll_JournalsFound_ReturnsSortedByDateDescending()
-        {
-            
-        }
-
-        [Fact]
-        public void Save_JournalSaved_ReturnsJournal()
+        public async Task GetAll_JournalsFound_ReturnsSortedByDateDescending()
         {
             
         }
 
         [Fact]
-        public void Save_JournalNull_ReturnsNull()
+        public async Task Save_JournalSaved_ReturnsJournal()
         {
             
         }
 
         [Fact]
-        public void Update_JournalUpdated_ReturnsJournal()
+        public async Task Save_JournalNull_ReturnsNull()
         {
             
         }
 
         [Fact]
-        public void Update_JournalNull_ReturnsNull()
+        public async Task Update_JournalUpdated_ReturnsJournal()
         {
             
         }
 
         [Fact]
-        public void Delete_JournalDeleted_ReturnsTrue()
+        public async Task Update_JournalNull_ReturnsNull()
         {
             
         }
 
         [Fact]
-        public void Delete_JournalNotFound_ReturnsFalse()
+        public async Task Delete_JournalDeleted_ReturnsTrue()
+        {
+            
+        }
+
+        [Fact]
+        public async Task Delete_JournalNotFound_ReturnsFalse()
         {
             
         }
