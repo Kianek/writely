a6349907f1510d8d745b0d021ece6819dd26f37d
Kianek <kallridge92@gmail.com>
2021-01-11 11:21:47 -0800

    Initial commit

---
+++ Writely.UnitTests/DatabaseFixture.cs
@@ -0,0 +1,38 @@
+using System.Data.Common;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Writely.Data;
+
+namespace Writely.UnitTests
+{
+    public class DatabaseFixture
+    {
+        public DbConnection Connection { get; private set; }
+
+        public DatabaseFixture()
+        {
+        }
+
+        public (DbContextOptions<AppDbContext>, 
+            OptionsWrapper<OperationalStoreOptions>) CreateDbContextOptions()
+        {
+            var connectionString = "Data Source=:memory:";
+            Connection = new SqliteConnection(connectionString);
+            Connection.Open();
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseSqlite(Connection).Options;
+            var operationalStoreOptions = 
+                new OptionsWrapper<OperationalStoreOptions>(
+                    new OperationalStoreOptions());
+            
+            return (options, operationalStoreOptions);
+        }
+
+        public void Dispose()
+        {
+            Connection?.Close();
+        }
+    }
+}
---
+++ Writely.UnitTests/RepositoryTestBase.cs
@@ -0,0 +1,23 @@
+using System.Threading.Tasks;
+using Writely.Data;
+
+namespace Writely.UnitTests
+{
+    public abstract class RepositoryTestBase
+    {
+        private readonly DatabaseFixture _fixture;
+        public AppDbContext Context { get; set; }
+
+        public RepositoryTestBase()
+        {
+            _fixture = new DatabaseFixture();
+        }
+
+        public async Task PrepareDatabase()
+        {
+            var (options, operationalStoreOptions) = _fixture.CreateDbContextOptions();
+            Context = new AppDbContext(options, operationalStoreOptions);
+            await Context.Database.EnsureCreatedAsync();
+        }
+    }
+}
