319e60388c8aef9b716bba51780218f6835f9094
Kianek <kallridge92@gmail.com>
2021-01-13 07:53:53 -0800

    Implement SortBy

--- Writely/Extensions/JournalExtensions.cs
+++ Writely/Extensions/JournalExtensions.cs
@@ -17,5 +17,20 @@ namespace Writely.Extensions
         {
             return journals.Select(j => j.ToDto()).ToList();
         }
+
+        public static IQueryable<Journal> SortBy(this IQueryable<Journal> journals, string? order = null)
+        {
+            switch (order)
+            {
+                case "asc":
+                    return journals.OrderBy(j => j.Title);
+                case "desc":
+                    return journals.OrderByDescending(j => j.Title);
+                case "date-asc":
+                    return journals.OrderBy(j => j.LastModified);
+                default:
+                    return journals.OrderByDescending(j => j.LastModified);
+            }
+        }
     }
 }
