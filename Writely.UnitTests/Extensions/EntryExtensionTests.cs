242f55b1781c98024c30a90564982a5de92b66c2
Kianek <kallridge92@gmail.com>
2021-01-14 15:07:46 -0800

    Passing tests

--- Writely.UnitTests/Extensions/EntryExtensionTests.cs
+++ Writely.UnitTests/Extensions/EntryExtensionTests.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using FluentAssertions;
 using Writely.Extensions;
+using Writely.Models;
 using Writely.Models.Dto;
 using Xunit;
 
@@ -34,6 +37,94 @@ namespace Writely.UnitTests.Extensions
             // Assert
             result.Should().BeOfType<List<EntryDto>>();
             result.Count.Should().Be(3);
+        }
+
+        [Fact]
+        public void SortBy_Default_SortByDateDescending()
+        {
+            // Arrange 
+            var entries = Helpers.GetEntries(3);
+            entries[0].LastModified = new DateTime(2021, 1, 14);
+            entries[1].LastModified = new DateTime(2021, 1, 19);
+            entries[2].LastModified = new DateTime(2021, 1, 25);
+
+            // Act
+            var result = entries.AsQueryable().SortBy().ToList();
+
+            // Assert
+            var first = result[0];
+            var second = result[1];
+            var third = result[2];
+            first.LastModified.Should().BeAfter(second.LastModified);
+            second.LastModified.Should().BeAfter(third.LastModified);
+        }
+        
+        [Fact]
+        public void SortBy_Default_SortByDateAscending()
+        {
+            // Arrange 
+            var entries = Helpers.GetEntries(3);
+            entries[0].LastModified = new DateTime(2021, 1, 14);
+            entries[1].LastModified = new DateTime(2021, 1, 19);
+            entries[2].LastModified = new DateTime(2021, 1, 25);
+
+            // Act
+            var result = entries.AsQueryable().SortBy(SortOrder.DateAscending).ToList();
+
+            // Assert
+            var first = result[0];
+            var second = result[1];
+            var third = result[2];
+            first.LastModified.Should().BeBefore(second.LastModified);
+            second.LastModified.Should().BeBefore(third.LastModified);
+        }
+        
+        [Fact]
+        public void SortBy_Default_SortByTitleDescending()
+        {
+            // Arrange
+            var entries = Helpers.GetEntries(3);
+            var skippy = "Skippy";
+            var flippy = "Flippy";
+            var zippy = "Zippy";
+            entries[0].Title = skippy;
+            entries[1].Title = flippy;
+            entries[2].Title = zippy;
+
+            // Act
+            var result = entries.AsQueryable().SortBy(SortOrder.Descending).ToList();
+
+            // Assert
+            var first = result[0];
+            var second = result[1];
+            var third = result[2];
+            first.Title.Should().Be(zippy);
+            second.Title.Should().Be(skippy);
+            third.Title.Should().Be(flippy);
+        }
+        
+        [Fact]
+        public void SortBy_Default_SortByTitleAscending()
+        {
+            // Arrange
+            var entries = Helpers.GetEntries(3);
+            var skippy = "Skippy";
+            var flippy = "Flippy";
+            var zippy = "Zippy";
+            entries[0].Title = skippy;
+            entries[1].Title = flippy;
+            entries[2].Title = zippy;
+
+            // Act
+            var result = entries.AsQueryable().SortBy(SortOrder.Ascending).ToList();
+
+            // Assert
+            var first = result[0];
+            var second = result[1];
+            var third = result[2];
+            first.Title.Should().Be(flippy);
+            second.Title.Should().Be(skippy);
+            third.Title.Should().Be(zippy);
         }
     }
 }
