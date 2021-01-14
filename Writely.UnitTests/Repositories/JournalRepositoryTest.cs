aa9c9c68d53ccc3fc79026db4e4936f143531119
Kianek <kallridge92@gmail.com>
2021-01-13 16:42:36 -0800

    Passing tests

--- Writely.UnitTests/Repositories/JournalRepositoryTest.cs
+++ Writely.UnitTests/Repositories/JournalRepositoryTest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Writely.Data;
@@ -81,37 +82,95 @@ namespace Writely.UnitTests.Repositories
         [Fact]
         public async Task Save_JournalSaved_ReturnsJournal()
         {
-            
+            // Arrange
+            await PrepareDatabase();
+            var journal = Helpers.GetJournal(_userId);
+            var repo = new JournalRepository(Context);
+
+            // Act
+           await repo.Save(journal);
+           var result = await Context.Journals.FindAsync(journal.Id);
+
+           // Assert
+           result.Should().NotBeNull();
         }
 
         [Fact]
-        public async Task Save_JournalNull_ReturnsNull()
+        public async Task Save_JournalNull_ThrowsArgumentNullException()
         {
-            
+            // Arrange
+            await PrepareDatabase();
+            var repo = new JournalRepository(Context);
+
+            // Assert
+            repo.Invoking(r => r.Save(null))
+                .Should()
+                .Throw<ArgumentNullException>();
         }
 
         [Fact]
         public async Task Update_JournalUpdated_ReturnsJournal()
         {
-            
+            // Arrange
+            await PrepareDatabase();
+            var journal = Helpers.GetJournal(_userId);
+            Context.Journals.Add(journal);
+            await Context.SaveChangesAsync();
+            var repo = new JournalRepository(Context);
+
+            // Act
+            journal.Title = "Better Title";
+            var result = await repo.Update(journal);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Title.Should().Be(journal.Title);
         }
 
         [Fact]
-        public async Task Update_JournalNull_ReturnsNull()
+        public async Task Update_JournalNull_ThrowsArgumentNullException()
         {
-            
+            // Arrange
+            await PrepareDatabase();
+            var repo = new JournalRepository(Context);
+
+            // Assert
+            repo.Invoking(r => r.Update(null))
+                .Should()
+                .Throw<ArgumentNullException>();
         }
 
         [Fact]
         public async Task Delete_JournalDeleted_ReturnsTrue()
         {
+            // Arrange
+            await PrepareDatabase();
+            var journal = Helpers.GetJournal(_userId);
+            Context.Journals.Add(journal);
+            await Context.SaveChangesAsync();
+            long id = journal.Id;
+            var repo = new JournalRepository(Context);
             
+
+            // Act
+            var result = await repo.Delete(id);
+
+            // Assert
+            result.Should().BeTrue();
         }
 
         [Fact]
         public async Task Delete_JournalNotFound_ReturnsFalse()
         {
-            
+            // Arrange
+            await PrepareDatabase();
+            var repo = new JournalRepository(Context);
+
+            // Act
+            var result = await repo.Delete(1L);
+
+            // Assert
+            result.Should().BeFalse();
         }
 
         private JournalRepository GetJournalRepo(AppDbContext context) => new JournalRepository(context);
