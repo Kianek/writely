346257464c688ec7ab9897bb5765bc60d9886973
Kianek <kallridge92@gmail.com>
2021-01-19 14:39:43 -0800

    Passing GetAllByTag tests

--- Writely.UnitTests/Repositories/EntryRepositoryTest.cs
+++ Writely.UnitTests/Repositories/EntryRepositoryTest.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Writely.Models;
@@ -82,13 +83,59 @@ namespace Writely.UnitTests.Repositories
         [Fact]
         public async Task GetAllByTag_JournalFound_ReturnsMatchingEntries()
         {
+            // Arrange
+            await PrepareDatabase();
+            var journal = Helpers.GetJournal();
+            var entries = Helpers.GetEntries(5);
+            entries[1].Tags = "dogs,cats";
+            entries[2].Tags = "cats";
+            entries[4].Tags = "frogs,dogs,cats,chickens";
+            AddEntriesToJournal(journal, entries);
+            Context.Journals.Add(journal);
+            await Context.SaveChangesAsync();
+            var repo = new EntryRepository(Context);
 
+            // Act
+            var result = await repo.GetAllByTag(journal.Id, new[] {"cats"});
+
+            // Assert
+            result.Count.Should().Be(3);
+            result[0].GetTags()?.Contains("cats").Should().BeTrue();
+            result[1].GetTags()?.Contains("cats").Should().BeTrue();
+            result[2].GetTags()?.Contains("cats").Should().BeTrue();
         }
 
         [Fact]
-        public async Task GetAllByTag_JournalFound_NoMatches_ReturnsNull()
+        public async Task GetAllByTag_JournalFound_NoMatches_ReturnsEmptyList()
         {
-            
+            // Arrange
+            await PrepareDatabase();
+            var journal = Helpers.GetJournal();
+            var entries = Helpers.GetEntries(3);
+            AddEntriesToJournal(journal, entries);
+            Context.Journals.Add(journal);
+            await Context.SaveChangesAsync();
+            var repo = new EntryRepository(Context);
+
+            // Act
+            var result = await repo.GetAllByTag(journal.Id, new[]{ "blah" });
+
+            // Assert
+            result.Count.Should().Be(0);
+        }
+
+        [Fact]
+        public async Task GetAllByTag_JournalNotFound_ReturnsNull()
+        {
+            // Arrange
+            await PrepareDatabase();
+            var repo = new EntryRepository(Context);
+
+            // Act
+            var result = await repo.GetAllByTag(1L, new[] {"Dogs"});
+
+            // Assert
+            result.Should().BeNull();
         }
 
         [Fact]
