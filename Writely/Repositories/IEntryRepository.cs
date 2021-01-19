19e76c67cbb6d11254efb0fab4e5544b9fa8b0d4
Kianek <kallridge92@gmail.com>
2021-01-19 14:35:18 -0800

    Tweak return type

--- Writely/Repositories/IEntryRepository.cs
+++ Writely/Repositories/IEntryRepository.cs
@@ -7,8 +7,8 @@ namespace Writely.Repositories
     public interface IEntryRepository
     {
         Task<Entry> GetById(long entryId);
-        Task<List<Entry>> GetAllByJournal(long journalId, string orderBy = "date-desc");
-        Task<List<Entry>> GetAllByTag(long journalId, string[] tags, string orderBy = "date-desc");
+        Task<List<Entry>?> GetAllByJournal(long journalId, string orderBy = "date-desc");
+        Task<List<Entry>?> GetAllByTag(long journalId, string[] tags, string orderBy = "date-desc");
         Task<Entry> Save(Entry entry);
         Task<Entry> Update(Entry entry);
         Task<bool> Delete(long journalId, long entryId);
