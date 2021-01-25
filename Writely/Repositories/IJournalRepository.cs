b7f84f701ecbc3bc7c78aa34ff9818fa4f78fefc
Kianek <kallridge92@gmail.com>
2021-01-25 15:07:03 -0800

    Tweak method name

--- Writely/Repositories/IJournalRepository.cs
+++ Writely/Repositories/IJournalRepository.cs
@@ -8,7 +8,7 @@ namespace Writely.Repositories
     public interface IJournalRepository
     {
         Task<Journal> GetById(long id);
-        Task<List<Journal>> GetAll(int limit = 0, string orderBy = "date-desc");
+        Task<List<Journal>> GetAllByUserId(string userId, int limit = 0, string orderBy = "date-desc");
         Task<Journal> Save(Journal journal);
         Task<Journal> Update(Journal updatedJournal);
         Task<bool> Delete(long id);
