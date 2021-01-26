534dbaf8f12286235b55564b036355d595c6fbbe
Kianek <kallridge92@gmail.com>
2021-01-26 10:41:33 -0800

    Implement Update method

--- Writely/Extensions/JournalExtensions.cs
+++ Writely/Extensions/JournalExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Writely.Models;
@@ -11,6 +12,23 @@ namespace Writely.Extensions
         
         public static List<JournalDto> MapToDto(this List<Journal> journals) 
             => journals.Select(j => j.ToDto()).ToList();
+
+        public static bool Update(this Journal journal, JournalUpdateModel model)
+        {
+            if (model is null)
+            {
+                throw new ArgumentNullException();
+            }
+                
+            var didUpdate = false;
+            if (model.Title is not null && journal.Title != model.Title)
+            {
+                journal.Title = model.Title;
+                didUpdate = true;
+            }
+            
+            return didUpdate;
+        }
 
         public static IQueryable<Journal> SortBy(this IQueryable<Journal> journals, string? order = null)
         {
