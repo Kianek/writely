e37ed770e17bff58822095f657402d03dd55eca9
Kianek <kallridge92@gmail.com>
2021-02-02 15:16:23 -0800

    Implement SortBy -> IOrderedEnumerable

--- Writely/Extensions/QueryExtensions.cs
+++ Writely/Extensions/QueryExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Writely.Models;
 
@@ -14,6 +15,16 @@ namespace Writely.Extensions
                 "desc" => query.OrderByDescending(entity => entity.Title),
                 "date-asc" => query.OrderBy(entity => entity.LastModified),
                 _ => query.OrderByDescending(entity => entity.LastModified)
+            };
+
+        public static IOrderedEnumerable<T> SortBy<T>(this IEnumerable<T> sequence, string order = "date-desc")
+            where T : ISortable
+            => order switch
+            {
+                "asc" => sequence.OrderBy(entity => entity.Title),
+                "desc" => sequence.OrderByDescending(entity => entity.Title),
+                "date-asc" => sequence.OrderBy(entity => entity.LastModified),
+                _ => sequence.OrderByDescending(entity => entity.LastModified),
             };
     }
 }
