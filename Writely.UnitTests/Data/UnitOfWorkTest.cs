22946817a87fc60b604089aa57ac85b0ede2c656
Kianek <kallridge92@gmail.com>
2021-02-04 17:25:47 -0800

    Passing test

--- Writely.UnitTests/Data/UnitOfWorkTest.cs
+++ Writely.UnitTests/Data/UnitOfWorkTest.cs
@@ -1,18 +1,29 @@
 using System.Threading.Tasks;
+using FluentAssertions;
+using Writely.Data;
 using Xunit;
 
 namespace Writely.UnitTests.Data
 {
     public class UnitOfWorkTest : DatabaseTestBase
     {
-        [Fact]
-        public async Task Complete_CanSaveChanges_ReturnsNumberOfChangesSaved()
+        [Theory]
+        [InlineData(3)]
+        [InlineData(6)]
+        [InlineData(10)]
+        public async Task Complete_CanSaveChanges_ReturnsNumberOfChangesSaved(int numOfJournals)
         {
             // Arrange
-            
+            await PrepareDatabase();
+            var journals = Helpers.GetJournals(numOfJournals);
+            var unitOfWork = new UnitOfWork(Context, "UserId");
+
             // Act
-            
+            unitOfWork.Journals.AddRange(journals);
+            var result = await unitOfWork.Complete();
+
             // Assert
+            result.Should().Be(numOfJournals);
         }
     }
 }
