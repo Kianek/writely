41164e419d4649a996bcc05985acb4f9641c3850
Kianek <kallridge92@gmail.com>
2021-02-06 15:03:11 -0800

    Update method signatures

--- Writely/Services/IEntryService.cs
+++ Writely/Services/IEntryService.cs
@@ -1,17 +1,17 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Writely.Models;
-using Writely.Models.Dto;
 
 namespace Writely.Services
 {
     public interface IEntryService
     {
-        Task<EntryDto> GetById(long entryId);
-        Task<List<EntryDto>> GetAllByJournal(long journalId);
-        Task<List<EntryDto>> GetAllByTag(long journalId, string tags);
-        Task<EntryDto> Create(Entry model);
-        Task<EntryDto> Update(long entryId, EntryUpdateModel updateModel);
-        Task Delete(long journalId, long entryId);
+        long JournalId { get; set; }
+        Task<Entry> GetById(long entryId);
+        Task<List<Entry>> GetAllByJournal(long journalId);
+        Task<List<Entry>> GetAllByTag(long journalId, string tags);
+        Task<Entry> Add(NewEntryModel model);
+        Task<Entry> Update(long entryId, EntryUpdateModel updateModel);
+        Task Remove(long entryId);
     }
 }
