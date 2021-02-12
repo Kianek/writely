6b8cca301214b6bc46eaa5fdac5f1f68f03d1e51
Kianek <kallridge92@gmail.com>
2021-02-12 12:18:35 -0800

    Implement Update method

--- Writely/Models/Entry.cs
+++ Writely/Models/Entry.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Writely.Data;
 
@@ -8,7 +9,7 @@ namespace Writely.Models
     {
         public string? Tags { get; set; }
         public string? Body { get; set; }
-        
+
         public long JournalId { get; set; }
         public Journal? Journal { get; set; }
 
@@ -35,13 +36,43 @@ namespace Writely.Models
                 : Tags = SanitizeTags(newTags);
         }
 
+        public bool Update(EntryUpdateModel model)
+        {
+            if (model is null)
+            {
+                throw new ArgumentNullException(nameof(model));
+            }
+
+            var updatedProperties = new Dictionary<string, bool>();
+            Title = Update(Title, model.Title, updatedProperties);
+            Tags = Update(Tags, model.Tags, updatedProperties);
+            Body = Update(Body, model.Body, updatedProperties);
+
+            var didUpdate = updatedProperties.Values.Any(value => value);
+            if (didUpdate) UpdateLastModified();
+
+            return didUpdate;
+        }
+
+        private string Update(string? property, string? updateProperty, Dictionary<string, bool> updatedProps)
+        {
+            if (updateProperty == null || property == updateProperty)
+            {
+                updatedProps[property!] = false;
+                return property;
+            }
+
+            updatedProps[property!] = true;
+            return updateProperty;
+        }
+
         public string[]? GetTags()
         {
             return Tags?.Split(",", StringSplitOptions.TrimEntries);
         }
 
         private static string SanitizeTags(string tags) =>
-                string.Join(",",
+            string.Join(",",
                 tags.Split(",", StringSplitOptions.RemoveEmptyEntries)
                     .Distinct()
                     .ToArray());
