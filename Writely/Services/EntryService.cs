fa8b2422988aa339cce1628a880612fe077f8b00
Kianek <kallridge92@gmail.com>
2021-02-12 17:03:18 -0800

    Implement Add method

--- Writely/Services/EntryService.cs
+++ Writely/Services/EntryService.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Writely.Data;
+using Writely.Exceptions;
 using Writely.Models;
 
 namespace Writely.Services
@@ -17,34 +18,57 @@ namespace Writely.Services
             JournalId = journalId;
         }
 
-        public Task<Entry> GetById(long entryId)
+        public async Task<Entry> GetById(long entryId)
+        {
+            return await GetUnitOfWork().Entries.GetById(entryId) 
+                   ?? throw new EntryNotFoundException($"Entry not found: {entryId}");
+        }
+
+        public async Task<IEnumerable<Entry>?> GetAll()
+        {
+            return await GetUnitOfWork().Entries.GetAll();
+        }
+
+        public async Task<IEnumerable<Entry>?> GetAllByTag(string tags, string order = "date-desc")
+        {
+            return await GetUnitOfWork().Entries.GetAllByTag(tags.Split(","), order);
+        }
+
+        public async Task<Entry> Add(NewEntryModel model)
+        {
+            if (JournalId == null)
+            {
+                throw new JournalNotFoundException();
+            }
+            
+            var unitOfWork = GetUnitOfWork();
+            var journal = await unitOfWork.Journals.GetById(JournalId.GetValueOrDefault());
+            if (journal == null)
+            {
+                throw new JournalNotFoundException();
+            }
+            var entry = new Entry
+            {
+                Title = model.Title,
+                Tags = model.Tags,
+                Body = model.Body
+            };
+            journal.Add(entry);
+            await unitOfWork.Complete();
+            
+            return entry;
+        }
+
+        public async Task<Entry> Update(long entryId, EntryUpdateModel updateModel)
         {
             throw new System.NotImplementedException();
         }
 
-        public Task<IEnumerable<Entry>> GetAll()
+        public async Task<Entry> Remove(long entryId)
         {
             throw new System.NotImplementedException();
         }
 
-        public Task<IEnumerable<Entry>> GetAllByTag(string tags, string order = "date-desc")
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public Task<Entry> Add(NewEntryModel model)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public Task<Entry> Update(long entryId, EntryUpdateModel updateModel)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public Task<Entry> Remove(long entryId)
-        {
-            throw new System.NotImplementedException();
-        }
+        private IUnitOfWork GetUnitOfWork() => new UnitOfWork(_context, null, JournalId);
     }
 }
