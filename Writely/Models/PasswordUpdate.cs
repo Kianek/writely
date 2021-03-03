477a1b614112ef091fcf5f53e37e32092d56e5e8
Kianek <kallridge92@gmail.com>
2021-03-03 12:21:22 -0800

    Add Deconstruct method

--- Writely/Models/PasswordUpdate.cs
+++ Writely/Models/PasswordUpdate.cs
@@ -5,6 +5,12 @@ namespace Writely.Models
         public string? Password { get; set; }
         public string? ConfirmPassword { get; set; }
 
+        public void Deconstruct(out string? password, out string? confirmPassword)
+        {
+            password = Password;
+            confirmPassword = ConfirmPassword;
+        }
+        
         public bool PasswordsMatch()
             => !(string.IsNullOrEmpty(Password) && string.IsNullOrEmpty(ConfirmPassword))
                 && Password == ConfirmPassword;
