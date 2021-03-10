402736cb22b70929b88eba19adbfcd1c0f27b807
Kianek <kallridge92@gmail.com>
2021-03-10 14:49:18 -0800

    Add IS4 config extension method

--- Writely/Extensions/WritelyServices.cs
+++ Writely/Extensions/WritelyServices.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.Extensions.DependencyInjection;
 using Writely.Data;
 using Writely.Services;
@@ -9,9 +10,21 @@ namespace Writely.Extensions
         public static void AddWritelyServices(this IServiceCollection services)
         {
             services.AddTransient<IUserService, UserService>();
-            services.AddTransient<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddTransient<IJournalService, JournalService>();
             services.AddTransient<IEntryService, EntryService>();
+        }
+
+        public static void AddIdentityServerAuthenticationWithJwt(this IServiceCollection services)
+        {
+            services.AddDefaultIdentity<AppUser>()
+                .AddEntityFrameworkStores<AppDbContext>();
+            
+            services.AddIdentityServer()
+                .AddApiAuthorization<AppUser, AppDbContext>();
+
+            services.AddAuthentication()
+                .AddIdentityServerJwt();
         }
     }
 }
