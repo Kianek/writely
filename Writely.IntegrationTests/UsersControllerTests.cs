66929f5e01c8502747ef66a62d4192d690b8eb09
Kianek <kallridge92@gmail.com>
2021-03-12 10:02:15 -0800

    Initial commit

---
+++ Writely.IntegrationTests/UsersControllerTests.cs
@@ -0,0 +1,95 @@
+using System.Threading.Tasks;
+using Writely.Models;
+using Xunit;
+
+namespace Writely.IntegrationTests
+{
+    public class UsersControllerTests : TestBase,  IClassFixture<WebAppFactory<Startup>>
+    {
+        
+        public UsersControllerTests(WebAppFactory<Startup> factory) : base(factory)
+        {
+        }
+
+        [Fact]
+        public async Task Register_NewUser_Returns201()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+        
+        [Fact]
+        public async Task Register_UserAlreadyRegistered_Returns400()
+        {
+            // Arrange
+            
+            // Act
+            
+            // Assert
+        }
+        
+        [Fact]
+        public async Task ChangeEmail_ChangeSuccessful_Returns201()
+        {
+            // Arrange
+            
+            // Act
+            
+            // Assert
+        }
+        [Fact]
+        
+        public async Task ChangeEmail_IncompleteAccountUpdate_Returns400()
+        {
+            // Arrange
+            
+            // Act
+            
+            // Assert
+        }
+        
+        [Fact]
+        public async Task ChangePassword_ChangeSuccessful_Returns201()
+        {
+            // Arrange
+            
+            // Act
+            
+            // Assert
+        }
+        
+        [Fact]
+        public async Task ChangePassword_IncompleteAccountUpdate_Returns400()
+        {
+            // Arrange
+            
+            // Act
+            
+            // Assert
+        }
+        [Fact]
+        
+        public async Task DeleteAccount_AccountDeleted_Returns200()
+        {
+            // Arrange
+            
+            // Act
+            
+            // Assert
+        }
+        
+        [Fact]
+        public async Task DeleteAccount_AccountNotFound_Returns400()
+        {
+            // Arrange
+            
+            // Act
+            
+            // Assert
+        }
+        
+    }
+}
