d22f316b7256336d49e699fc8445cb6e602f0013
Kianek <kallridge92@gmail.com>
2021-03-15 11:41:52 -0700

    Add tests

--- Writely.UnitTests/Controllers/UsersControllerTests.cs
+++ Writely.UnitTests/Controllers/UsersControllerTests.cs
@@ -13,12 +13,6 @@ namespace Writely.UnitTests.Controllers
 {
     public class UsersControllerTests
     {
-        private readonly IUserService _service;
-        
-        public UsersControllerTests()
-        {
-        }
-
         [Fact]
         public async Task Register_NewUser_ReturnsCreated()
         {
@@ -45,81 +39,114 @@ namespace Writely.UnitTests.Controllers
         public async Task Register_IncompleteRegistration_ReturnsBadRequest()
         {
             // Arrange
-            // var registration = 
-            
+            var registration = Helpers.GetRegistration();
+            registration.FirstName = null;
+            registration.ConfirmPassword = null;
+            var controller = PrepControllerWithoutUser();
+
             // Act
-            
+            var response = await controller.Register(registration);
+
             // Assert
+            response.Should().BeOfType<BadRequestResult>();
         }
         
         [Fact]
         public async Task Register_UserAlreadyRegistered_ReturnsBadRequest()
         {
             // Arrange
-            
+            var registration = Helpers.GetRegistration();
+            var controller = PrepControllerWithUser();
+
             // Act
-            
+            var response = await controller.Register(registration);
+
             // Assert
+            response.Should().BeOfType<BadRequestResult>();
         }
         
         [Fact]
-        public async Task ChangeEmail_ChangeSuccessful_ReturnsCreated()
+        public async Task ChangeEmail_ChangeSuccessful_ReturnsNoContent()
         {
             // Arrange
-            
+            var emailUpdate = Helpers.GetEmailUpdate();
+            var controller = PrepUserServiceWithUser();
+
             // Act
-            
+            var response = await controller.ChangeEmail(emailUpdate);
+
             // Assert
+            response.Should().BeOfType<NoContentResult>();
         }
-        [Fact]
         
+        [Fact]
         public async Task ChangeEmail_IncompleteAccountUpdate_ReturnsBadRequest()
         {
             // Arrange
-            
+            var accountUpdate = Helpers.GetEmailUpdate();
+            accountUpdate.EmailUpdate!.Email = null;
+            var controller = PrepControllerWithUser();
+
             // Act
-            
+            var response = await controller.ChangeEmail(accountUpdate);
+
             // Assert
+            response.Should().BeOfType<BadRequestResult>();
         }
         
         [Fact]
         public async Task ChangePassword_ChangeSuccessful_ReturnsCreated()
         {
             // Arrange
-            
+            var accountUpdate = Helpers.GetPasswordUpdate();
+            var controller = PrepControllerWithUser();
+
             // Act
-            
+            var response = await controller.ChangePassword(accountUpdate);
+
             // Assert
+            response.Should().BeOfType<OkResult>();
         }
         
         [Fact]
         public async Task ChangePassword_IncompleteAccountUpdate_ReturnsBadRequest()
         {
             // Arrange
-            
+            var accountUpdate = Helpers.GetPasswordUpdate();
+            accountUpdate.PasswordUpdate!.Password = "Goobledyblech";
+            var controller = PrepControllerWithUser();
+
             // Act
-            
+            var response = await controller.ChangePassword(accountUpdate);
+
             // Assert
+            response.Should().BeOfType<BadRequestResult>();
         }
-        [Fact]
         
+        [Fact]
         public async Task DeleteAccount_AccountDeleted_ReturnsOk()
         {
             // Arrange
-            
+            var controller = PrepControllerWithUser();
+
             // Act
-            
+            var response = await controller.DeleteAccount("UserId");
+
             // Assert
+            response.Should().BeOfType<OkResult>();
         }
         
         [Fact]
         public async Task DeleteAccount_AccountNotFound_ReturnsBadRequest()
         {
             // Arrange
-            
+            var controller = PrepControllerWithUser();
+
             // Act
-            
+            var response = await controller.DeleteAccount("UserIdDelete");
+
             // Assert
+            response.Should().BeOfType<BadRequestResult>();
         }
 
         private IUserService PrepUserServiceWithoutUser()
@@ -136,13 +163,19 @@ namespace Writely.UnitTests.Controllers
             var userService = GetMockUserService();
             userService.Setup(us => us.CreateAccount(It.IsAny<Registration>()))
                 .Throws<DuplicateUserException>();
+            userService.Setup(us => us.DeleteAccount("UserId"))
+                .ReturnsAsync(() => IdentityResult.Success);
+            userService.Setup(us => us.DeleteAccount("UserIdDelete"))
+                .ReturnsAsync(() => IdentityResult.Failed());
             
             return userService.Object;
         }
 
         private UsersController PrepControllerWithoutUser()
             => new (PrepUserServiceWithoutUser());
-        
+
+        private UsersController PrepControllerWithUser()
+            => new(PrepUserServiceWithUser());
 
         private Mock<IUserService> GetMockUserService() => new ();
     }
