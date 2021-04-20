5cee469e64c82a5633a4486afd975d1db7a83ebe
Kianek <kallridge92@gmail.com>
2021-04-20 12:06:27 -0700

    Minor formatting

--- Writely/Services/IJournalService.cs
+++ Writely/Services/IJournalService.cs
@@ -8,8 +8,11 @@ namespace Writely.Services
     public interface IJournalService
     {
         string? UserId { get; set; }
+        
         Task<Journal> GetById(long journalId);
+        
         Task<IEnumerable<Journal>?> GetAll(int limit = 0, string orderBy = "date-desc");
+        
         Task<Journal> Add(NewJournal model);
 
         Task<int> Update(long journalId, JournalUpdate updateModel);
