3df1c2cab27e031b1710868c125f24fbd14d6d59
Kianek <kallridge92@gmail.com>
2021-04-22 11:06:31 -0700

    Add properties

--- Writely/Data/IUnitOfWork.cs
+++ Writely/Data/IUnitOfWork.cs
@@ -7,6 +7,8 @@ namespace Writely.Data
 {
     public interface IUnitOfWork : IDisposable
     {
+        string? UserId { get; set; }
+        long? JournalId { get; set; }
         IRepository<Journal> Journals { get; }
         IEntryRepository Entries { get; }
         Task<int> Complete();
