059377276c83164f51af4c49b512b819349740a8
Kianek <kallridge92@gmail.com>
2021-04-22 14:59:06 -0700

    Replace params with QueryFilter

--- Writely/Repositories/IRepository.cs
+++ Writely/Repositories/IRepository.cs
@@ -1,19 +1,15 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
+using Writely.Models;
 
 namespace Writely.Repositories
 {
     public interface IRepository<TEntity> where TEntity : class
     {
         Task<TEntity?> GetById(long id);
-        Task<IEnumerable<TEntity>?> GetAll(
-            Expression<Func<TEntity, bool>>? filter = null,
-            string? orderBy = null,
-            int limit = 0);
+        Task<IEnumerable<TEntity>?> GetAll(QueryFilter? filter = null);
         Task<TEntity?> Find(Expression<Func<TEntity, bool>> predicate);
         void Add(TEntity entity);
         void AddRange(IEnumerable<TEntity> entities);
