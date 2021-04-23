f26717e43cfe4a7c9b029e1762662d362deb4fdf
Kianek <kallridge92@gmail.com>
2021-04-22 18:32:32 -0700

    Remove GetAll & GetAllByTag

--- Writely/Services/IEntryService.cs
+++ Writely/Services/IEntryService.cs
@@ -8,8 +8,6 @@ namespace Writely.Services
     {
         long? JournalId { get; set; }
         Task<Entry> GetById(long entryId);
-        Task<IEnumerable<Entry>?> GetAll();
-        Task<IEnumerable<Entry>?> GetAllByTag(string tags, string orderBy = "date-desc");
         Task<Entry> Add(NewEntry model);
         Task<Entry> Update(long entryId, EntryUpdate updateModel);
         Task<Entry> Remove(long entryId);
