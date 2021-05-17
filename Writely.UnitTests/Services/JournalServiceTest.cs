0f6d9fff3525b6400ab7246a86896ca646d417c6
Kianek <kallridge92@gmail.com>
2021-05-17 15:01:57 -0700

    Add additional tests

--- Writely.UnitTests/Services/JournalServiceTest.cs
+++ Writely.UnitTests/Services/JournalServiceTest.cs
@@ -13,15 +13,17 @@ namespace Writely.UnitTests.Services
 {
     public class JournalServiceTest : DatabaseTestBase
     {
+        private JournalService _service;
+        
         [Fact]
         public async Task GetById_UserFound_ReturnsJournal()
         {
             // Arrange
             var journal = await PrepDbWithJournal();
-            var service = GetJournalService();
+            _service = GetJournalService();
 
             // Act
-            var result = await service.GetById(journal.Id);
+            var result = await _service.GetById(journal.Id);
 
             // Assert
             result.Should().NotBeNull();
@@ -33,10 +35,10 @@ namespace Writely.UnitTests.Services
         {
             // Arrange
             await PrepareDatabase();
-            var service = GetJournalService();
+            _service = GetJournalService();
 
             // Assert
-            service.Invoking(s => s.GetById(2L))
+            _service.Invoking(s => s.GetById(2L))
                 .Should()
                 .Throw<JournalNotFoundException>();
         }
@@ -46,10 +48,10 @@ namespace Writely.UnitTests.Services
         {
             // Arrange
             await PrepareDatabase();
-            var service = GetJournalService(null);
+            _service = GetJournalService("");
 
             // Assert
-            service.Invoking(s => s.GetById(1L))
+            _service.Invoking(s => s.GetById(1L))
                 .Should()
                 .Throw<UserNotFoundException>();
         }
@@ -62,10 +64,10 @@ namespace Writely.UnitTests.Services
             var journals = Helpers.GetJournals(5);
             Context?.Journals!.AddRange(journals);
             await Context!.SaveChangesAsync();
-            var service = GetJournalService();
+            _service = GetJournalService();
 
             // Act
-            var result = await service.GetAll(new QueryFilter());
+            var result = await _service.GetAll(new QueryFilter());
 
             // Assert
             result.Should().HaveCount(5);
@@ -76,37 +78,37 @@ namespace Writely.UnitTests.Services
         {
             // Arrange
             await PrepareDatabase();
-            var service = GetJournalService(null);
+            _service = GetJournalService(null);
 
             // Assert
-            service.Invoking(s => s.GetAll(new QueryFilter()))
+            _service.Invoking(s => s.GetAll(new QueryFilter()))
                 .Should()
                 .Throw<UserNotFoundException>();
         }
 
         [Fact]
-        public async Task GetEntriesByJournal_EntriesFound_ReturnsEntries()
+        public async Task GetAllEntries_EntriesFound_ReturnsEntries()
         {
             // Arrange
             var journal = await PrepDbWithJournal();
-            var service = GetJournalService();
+            _service = GetJournalService();
 
             // Act
-            var result = (await service.GetEntriesByJournal(journal.Id, new QueryFilter()))?.ToList();
+            var result = (await _service.GetAllEntries(journal.Id, new QueryFilter()))?.ToList();
 
             // Assert
             result?.Count.Should().Be(5);
         }
 
         [Fact]
-        public async Task GetEntriesByJournal_JournalNotFound_ThrowsJournalNotFoundException()
+        public async Task GetAllEntries_JournalNotFound_ThrowsJournalNotFoundException()
         {
             // Arrange
             await PrepareDatabase();
-            var service = GetJournalService();
+            _service = GetJournalService();
 
             // Assert
-            service.Invoking(s => s.GetEntriesByJournal(9L, new QueryFilter()))
+            _service.Invoking(s => s.GetAllEntries(9L, new QueryFilter()))
                 .Should()
                 .Throw<JournalNotFoundException>();
         }
@@ -116,11 +118,11 @@ namespace Writely.UnitTests.Services
         {
             // Arrange
             await PrepareDatabase();
-            var service = GetJournalService();
             var newJournal = new NewJournal {Title = "Fancy Journal"};
+            _service = GetJournalService();
 
             // Act
-            var result = await service.Add(newJournal);
+            var result = await _service.Add(newJournal);
 
             // Assert
             result.Should().NotBeNull();
@@ -133,10 +135,10 @@ namespace Writely.UnitTests.Services
         {
             // Arrange
             await PrepareDatabase();
-            var service = GetJournalService();
+            _service = GetJournalService();
 
             // Assert
-            service.Invoking(s => s.Add(null!))
+            _service.Invoking(s => s.Add(null!))
                 .Should()
                 .Throw<ArgumentNullException>();
         }
@@ -146,10 +148,10 @@ namespace Writely.UnitTests.Services
         {
             // Arrange
             await PrepareDatabase();
-            var service = GetJournalService(null);
+            _service = GetJournalService(null);
 
             // Assert
-            service.Invoking(s => s.Add(It.IsAny<NewJournal>()))
+            _service.Invoking(s => s.Add(It.IsAny<NewJournal>()))
                 .Should()
                 .Throw<UserNotFoundException>();
         }
@@ -160,10 +162,10 @@ namespace Writely.UnitTests.Services
             // Arrange
             var journal = await PrepDbWithJournal();
             var updateModel = new JournalUpdate {Title = "Shiny New Title"};
-            var service = GetJournalService(journal.UserId);
+            _service = GetJournalService();
 
             // Act
-            var result = await service.Update(journal.Id, updateModel);
+            var result = await _service.Update(journal.Id, updateModel);
 
             // Assert
             result.Should().Be(1);
@@ -174,10 +176,10 @@ namespace Writely.UnitTests.Services
         {
             // Arrange
             await PrepDbWithJournal();
-            var service = GetJournalService();
+            _service = GetJournalService();
 
             // Assert
-            service.Invoking(s => s.Update(1L, null!))
+            _service.Invoking(s => s.Update(1L, null!))
                 .Should()
                 .Throw<ArgumentNullException>();
         }
@@ -187,10 +189,10 @@ namespace Writely.UnitTests.Services
         {
             // Arrange
             await PrepareDatabase();
-            var service = GetJournalService();
+            _service = GetJournalService();
 
             // Assert
-            service.Invoking(s => s.Update(1L, new JournalUpdate()))
+            _service.Invoking(s => s.Update(1L, new JournalUpdate()))
                 .Should()
                 .Throw<JournalNotFoundException>();
         }
@@ -200,10 +202,10 @@ namespace Writely.UnitTests.Services
         {
             // Arrange
             var journal = await PrepDbWithJournal();
-            var service = GetJournalService();
+            _service = GetJournalService();
 
             // Act
-            var result = await service.Remove(journal.Id);
+            var result = await _service.Remove(journal.Id);
 
             // Assert
             result.Should().BeGreaterOrEqualTo(1);
@@ -214,10 +216,10 @@ namespace Writely.UnitTests.Services
         {
             // Arrange
             await PrepareDatabase();
-            var service = GetJournalService();
+            _service = GetJournalService();
 
             // Assert
-            service.Invoking(s => s.Remove(1L))
+            _service.Invoking(s => s.Remove(1L))
                 .Should()
                 .Throw<JournalNotFoundException>();
         }
@@ -227,21 +229,210 @@ namespace Writely.UnitTests.Services
         {
             // Arrange
             await PrepareDatabase();
-            var service = GetJournalService();
             var journals = Helpers.GetJournals(3);
             Helpers.AddEntriesToJournal(journals[2], Helpers.GetEntries(20));
-            Context.Journals.AddRange(journals);
+            Context!.Journals?.AddRange(journals);
             await Context.SaveChangesAsync();
+            _service = GetJournalService();
 
             // Act
-            var result = await service.RemoveAllByUser("UserId");
+            var result = await _service.RemoveAllByUser("UserId");
 
             // Assert
             result.Should().BeGreaterOrEqualTo(20);
         }
+        
+        // Entry-related tests
 
+        [Fact]
+        public async Task GetEntry_EntryFound_ReturnsEntry()
+        {
+            // Arrange
+            var journal = await PrepDbWithJournal();
+            _service = GetJournalService();
+            
+            // Act
+            var result = await _service.GetEntry(journal.Id, 1L);
+
+            // Assert
+            result.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task GetEntry_EntryNotFound_ThrowsEntryNotFoundException()
+        {
+            // Arrange
+            var journal = await PrepDbWithJournal();
+            _service = GetJournalService();
+
+            // Assert
+            _service.Invoking(s => s.GetEntry(journal.Id, 9L))
+                .Should()
+                .Throw<EntryNotFoundException>();
+        }
+
+        [Fact]
+        public async Task GetEntry_JournalNotFound_ThrowsJournalNotFoundException()
+        {
+            // Arrange
+            await PrepareDatabase();
+            _service = GetJournalService();
+            
+            // Assert
+            _service.Invoking(s => s.GetEntry(5L, 1L))
+                .Should()
+                .Throw<JournalNotFoundException>();
+        }
+
+        [Fact]
+        public async Task AddEntry_EntryAdded_ReturnsEntry()
+        {
+            // Arrange
+            var journal = await PrepDbWithJournal();
+            var newEntry = GetValidEntry(journal.UserId, journal.Id);
+            _service = GetJournalService();
+
+            // Act
+            var result = await _service.AddEntry(journal.Id, newEntry);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Title.Should().Be(newEntry.Title);
+        }
+
+        [Fact]
+        public async Task AddEntry_JournalNotFound_ThrowsJournalNotFoundException()
+        {
+            // Arrange
+            await PrepareDatabase();
+            _service = GetJournalService();
+
+            // Assert
+            _service.Invoking(s => s.AddEntry(4L, GetValidEntry("UserId", 5L)))
+                .Should()
+                .Throw<JournalNotFoundException>();
+        }
+
+        [Fact]
+        public async Task AddEntry_NewEntryNull_ThrowsArgumentNullException()
+        {
+            // Arrange
+            var journal = await PrepDbWithJournal();
+            _service = GetJournalService();
+
+            // Assert
+            _service.Invoking(s => s.AddEntry(journal.Id, null!))
+                .Should()
+                .Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public async Task UpdateEntry_JournalAndEntryFound_EntryUpdated_ReturnsEntry()
+        {
+            // Arrange
+            var journal = await PrepDbWithJournal();
+            var entryId = journal.Entries[0].Id;
+            var update = new EntryUpdate { Body = "Spiffy new body" };
+            _service = GetJournalService();
+
+            // Act
+            var result = await _service.UpdateEntry(journal.Id, entryId, update);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Body.Should().Be(update.Body);
+        }
+
+        [Fact]
+        public async Task UpdateEntry_JournalNotFound_ThrowsJournalNotFoundException()
+        {
+            // Arrange
+            await PrepareDatabase();
+            _service = GetJournalService();
+
+            // Act
+            _service.Invoking(s => s.UpdateEntry(4L, 2L, new EntryUpdate()))
+                .Should()
+                .Throw<JournalNotFoundException>();
+        }
+
+        [Fact]
+        public async Task UpdateEntry_EntryNotFound_ThrowsEntryNotFoundException()
+        {
+            // Arrange
+            var journal = await PrepDbWithJournal();
+            _service = GetJournalService();
+
+            // Act
+            _service.Invoking(s => s.UpdateEntry(journal.Id, 10L, new EntryUpdate()))
+                .Should()
+                .Throw<EntryNotFoundException>();
+        }
+
+        [Fact]
+        public async Task UpdateEntry_EntryUpdateNull_ThrowsArgumentNullException()
+        {
+            // Arrange
+            var journal = await PrepDbWithJournal();
+            var entryId = journal.Entries[0].Id;
+            _service = GetJournalService();
+
+            // Assert
+            _service.Invoking(s => s.UpdateEntry(journal.Id, entryId, null!))
+                .Should()
+                .Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public async Task RemoveEntry_JournalAndEntryFound_EntryRemoved_ReturnsEntry()
+        {
+            // Arrange
+            var journal = await PrepDbWithJournal();
+            var entryId = journal.Entries[0].Id;
+            _service = GetJournalService();
+
+            // Act
+            var result = await _service.RemoveEntry(journal.Id, entryId);
+
+            // Assert
+            journal.Entries.Should().NotContain(result);
+        }
+
+        [Fact]
+        public async Task RemoveEntry_JournalNotFound_ThrowsJournalNotFoundException()
+        {
+            // Arrange
+            await PrepareDatabase();
+            _service = GetJournalService();
+
+            // Assert
+            _service.Invoking(s => s.RemoveEntry(3L, 4L))
+                .Should()
+                .Throw<JournalNotFoundException>();
+        }
+        
+        [Fact]
+        public async Task RemoveEntry_JournalNotFound_ThrowsEntryNotFoundException()
+        {
+            // Arrange
+            var journal = await PrepDbWithJournal();
+            _service = GetJournalService();
+
+            // Assert
+            _service.Invoking(s => s.RemoveEntry(journal.Id, 10L))
+                .Should()
+                .Throw<EntryNotFoundException>();
+        }
+        
+        private NewEntry GetValidEntry(string userId, long journalId) => new NewEntry(
+                                                   userId,
+                                                   journalId,
+                                                   "New Entry",
+                                                   "one,two,three",
+                                                   "Some text here");
+        
         private JournalService GetJournalService(string? userId = "UserId") 
-            => new(Context!) { UserId = userId};
+            => new(Context!, new EntryService()) { UserId = userId};
 
         private async Task<Journal> PrepDbWithJournal()
         {
