f76c97c9fecc73654586de32ba3e9c5b4735aa31
Kianek <kallridge92@gmail.com>
2021-07-20 20:12:25 -0700

    Inherit from AccountUpdate; add ctor

--- Writely/Models/PasswordUpdate.cs
+++ Writely/Models/PasswordUpdate.cs
@@ -1,9 +1,16 @@
 namespace Writely.Models
 {
-    public class PasswordUpdate
+    public class PasswordUpdate : AccountUpdate
     {
         public string? CurrentPassword { get; set; }
         public string? NewPassword { get; set; }
+        
+        public PasswordUpdate(string userId, string? currentPassword, string? newPassword) 
+            : base(userId)
+        {
+            CurrentPassword = currentPassword;
+            NewPassword = newPassword;
+        }
 
         public void Deconstruct(out string? password, out string? newPassword)
         {
