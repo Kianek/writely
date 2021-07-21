e97a4cd92269e86ff51d43843f462ae29188f22e
Kianek <kallridge92@gmail.com>
2021-07-20 20:11:30 -0700

    Make abstract class

--- Writely/Models/AccountUpdate.cs
+++ Writely/Models/AccountUpdate.cs
@@ -1,17 +1,9 @@
 namespace Writely.Models
 {
-    public class AccountUpdate
+    public abstract class AccountUpdate
     {
         public string UserId { get; set; }
-        public EmailUpdate? EmailUpdate { get; set; }
-        public PasswordUpdate? PasswordUpdate { get; set; }
-
-        public AccountUpdate(string userId, EmailUpdate emailUpdate = null!,
-            PasswordUpdate passwordUpdate = null!)
-        {
-            UserId = userId;
-            EmailUpdate = emailUpdate;
-            PasswordUpdate = passwordUpdate;
-        }
+        
+        public AccountUpdate(string userId) => UserId = userId;
     }
 }
